ae3a3d914008e7cba091f22ea1be52c1a9825dbd
Vishnu V <palebluedotlabs@gmail.com>
2024-01-19 02:34:07 +0530

    added job role in res

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -13,9 +13,34 @@ const getLogin = async (req, res) =>{
         let query = `SELECT EM.EmployeeId, EM.EmployeeName, U.EmailId, DM.DepartmentName, RM.RoleName FROM userpool AS U JOIN employeemaster AS EM ON EM.Id = U.refEmployeeMaster JOIN departmentmaster AS DM ON DM.Id = EM.refDepartment JOIN rolemaster AS RM ON RM.Id = EM.refRole WHERE U.EmailId = ? AND U.password =?;`;
         let params = [email_id, password];
         let result = await executeQuery(query, params);
-        console.info(result)
+        let jobRole;
         if(result.length > 0){
-            res.status(200).json({message : "Login successfull",employee_id: result[0].EmployeeId, name:result[0].EmployeeName, email: result[0].EmailId,  role : result[0].RoleName, department: result[0].DepartmentName, })
+            switch (result[0].RoleName) {
+                case "Admin":
+                    jobRole = "admin";
+                    break;
+                case "Hod":
+                    switch (result[0].DepartmentName) {
+                        case "Design":
+                            jobRole = "design_hod";
+                            break;
+                        case "Purchase":
+                            jobRole = "purchase_hod";
+                            break;
+                    }
+                    break;
+                case "Employee":
+                    switch (result[0].DepartmentName) {
+                        case "Design":
+                            jobRole = "design_em";
+                            break;
+                        case "Purchase":
+                            jobRole = "purchase_em";
+                            break;
+                    }
+                    break;
+            }
+            res.status(200).json({message : "Login successfull",employee_id: result[0].EmployeeId, name:result[0].EmployeeName, email: result[0].EmailId,  role : result[0].RoleName, department: result[0].DepartmentName, job_role: jobRole })
         }else{
             res.status(404).json({message: 'User Not Found!'})
         }
